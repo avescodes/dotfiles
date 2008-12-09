c971cab9f50a358427feddf6a77e8b20d54f0b1f
Ryan Neufeld <neufelry@gmail.com>
2008-12-08 23:11:38 -0600

    Added a neat smart-tab function I found on the web for completions and indents on one tab

--- emacs-init.el
+++ emacs-init.el
@@ -1,7 +1,7 @@
 ;; File:     ~/.emacs.d/emacs-init.el
 ;; Author:   Ryan Neufeld <neufelry@gmail.com>
 ;; Forked from: Burke Libbey <burke@burkelibbey.org>
-;; Modified: <2008-12-07 23:33:04 CST>
+;; Modified: <2008-12-08 23:11:05 CST>
 
 ;; This assumes ~/.emacs contains '(load "~/.emacs.d/emacs-init.el")'
 
@@ -325,5 +325,41 @@
                            (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
 
 (setq debug-on-error nil)
+
+
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+"This smart tab is minibuffer compliant: it acts as usual in
+the minibuffer. Else, if mark is active, indents region. Else if
+point is at the end of a symbol, expands it. Else indents the
+current line."
+(interactive)
+(if (minibufferp)
+(unless (minibuffer-complete)
+(dabbrev-expand nil))
+(if mark-active
+(indent-region (region-beginning)
+(region-end))
+(if (looking-at "\\_>")
+(dabbrev-expand nil)
+(indent-for-tab-command)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
