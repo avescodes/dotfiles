05344aa9f4256249232a7ca62e23bfe48494da1c
Ryan Neufeld <neufelry@gmail.com>
2008-12-10 19:24:34 -0600

    Added ruby-electric hook for ruby mode

--- emacs-init.el
+++ emacs-init.el
@@ -1,7 +1,7 @@
 ;; File:     ~/.emacs.d/emacs-init.el
 ;; Author:   Ryan Neufeld <neufelry@gmail.com>
 ;; Forked from: Burke Libbey <burke@burkelibbey.org>
-;; Modified: <2008-12-08 23:11:05 CST>
+;; Modified: <2008-12-10 19:24:13 CST>
 
 ;; This assumes ~/.emacs contains '(load "~/.emacs.d/emacs-init.el")'
 
@@ -238,6 +238,21 @@
 
 ;; Ruby
 (add-hook 'ruby-mode-hook     'set-newline-and-indent)
+(add-hook 'ruby-mode-hook
+          (lambda()
+            (add-hook 'local-write-file-hooks
+                      '(lambda()
+                         (save-excursion
+                           (untabify (point-min) (point-max))
+                           (delete-trailing-whitespace)
+                           )))
+            (set (make-local-variable 'indent-tabs-mode) 'nil)
+            (set (make-local-variable 'tab-width) 2)
+            (imenu-add-to-menubar "IMENU")
+            (define-key ruby-mode-map "C-m" 'newline-and-indent) ;Not sure if this line is 100% right but it works!
+            (require 'ruby-electric)
+            (ruby-electric-mode t)
+            ))
 ;(add-hook 'ruby-mode-hook     'enable-rct)
 ;;This isn't working
 (add-hook 'lisp-mode          'set-newline-and-indent)
