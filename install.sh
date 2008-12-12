1219de4b12efbab8a09064124065b5f8248e321b
Ryan Neufeld <neufelry@gmail.com>
2008-12-12 10:41:38 -0600

    Fixed ugly error message in install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 cd ~/.emacs.d/installable || exit
-for FILE in .??*; do
-  echo "Installing $FILE..."
+for FILE in *; do
+  echo "Installing .$FILE..."
   mv ~/.$FILE ~/.$FILE.old >/dev/null 2>&1
   cp ~/.emacs.d/installable/$FILE ~/.$FILE
 done
 
-mkdir $HOME/.clojure
+cd $HOME/.clojure || mkdir $HOME/.clojure 
+echo "Installing jline.jar to ~/.clojure/"
 cp $CLJ_DIR/jline.jar $HOME/.clojure/jline.jar
 echo "Done!"
