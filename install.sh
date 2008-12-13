b78eb8e7a3cf312d40fbb73a38bcdd214ee4d3d1
Ryan Neufeld <neufelry@gmail.com>
2008-12-12 20:36:54 -0600

    Revert "Fixed ugly error message in install.sh"
    This reverts commit 1219de4b12efbab8a09064124065b5f8248e321b.

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 cd ~/.emacs.d/installable || exit
-for FILE in *; do
-  echo "Installing .$FILE..."
+for FILE in .??*; do
+  echo "Installing $FILE..."
   mv ~/.$FILE ~/.$FILE.old >/dev/null 2>&1
   cp ~/.emacs.d/installable/$FILE ~/.$FILE
 done
 
-cd $HOME/.clojure || mkdir $HOME/.clojure 
-echo "Installing jline.jar to ~/.clojure/"
+mkdir $HOME/.clojure
 cp $CLJ_DIR/jline.jar $HOME/.clojure/jline.jar
 echo "Done!"
