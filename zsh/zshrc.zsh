af701a1243c5421113eb88356e7c41f5fe5d931e
Ryan Neufeld <>
2010-10-14 15:37:03 -0500

    Fixed over-ridden blank git commit email

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -14,8 +14,6 @@ fi
 
 export GIT_AUTHOR_NAME=$NAME
 export GIT_COMMITTER_NAME=$NAME
-export GIT_AUTHOR_EMAIL=$EMAIL
-export GIT_COMMITTER_EMAIL=$EMAIL
 export RUBYOPT=""
 export EDITOR="pico"
 export BROWSER="w3m"
