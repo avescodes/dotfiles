facc7642dcdc6a8d82ac13cd8cf2ecef9b77d1b8
Ryan Neufeld <rkneufeld@gmail.com>
2010-11-02 23:34:13 -0500

    RPS1 is back in my life

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -10,5 +10,7 @@ function build-prompt() {
 }
 PS1='`build-prompt`'
 
+RPS1="%{$fg[cyan]%}%n %{$reset_color%}@ %{$fg_bold[green]%}%U%m%u %{$reset_color%}(%T)"
+
 [ $TERM = "eterm-color" ] && setopt singlelinezle
-[ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+[ $TERM = "dumb" ] && unsetopt zle && PS1='$ ' && RPS1=''
