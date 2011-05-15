d1505a196e55bc8311b32b2486f281d094a6719b
Ryan Neufeld <ryan@ryanneufeld.ca>
2011-05-15 19:09:14 -0400

    Cleaned up zsh/paths.zsh and added vim-scala bundle

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,17 +9,13 @@ setopt ALL_EXPORT
 MANPATH=/opt/local/share/man:$MANPATH
 
 # DB bins
-PATH=/opt/local/lib/postgresql83/bin/:/usr/local/mysql/bin:$PATH 
+PATH=/usr/local/mysql/bin:$PATH 
 
 # User Dir bins
-PATH=$HOME/bin:$HOME/bin/checker:$HOME/opt/scala/bin:$PATH
-PATH="$HOME/.emacs.d/clojure:$HOME/.emacs.d/site-lisp/ri:$PATH"
+PATH=$HOME/bin:$HOME/bin/checker:$PATH
 
 # Port, then homebrew
-PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 PATH="/usr/local/bin:$PATH"
-
-CLJ_DIR="$HOME/.emacs.d/clojure"
 
 CLASSPATH=.:$CLASSPATH
 unsetopt ALL_EXPORT
