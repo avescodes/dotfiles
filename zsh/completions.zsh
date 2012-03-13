c19d69b8e657abb9222e27c7fa98f39657905867
Ryan Neufeld <ryan@thinkrelevance.com>
2012-03-13 17:35:21 -0500

    Updates

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -6,6 +6,8 @@
 zmodload -i zsh/complist
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
+# Include known hosts, requires "HashKnownHosts no" in .ssh/config -http://www.sourceguru.net/ssh-host-completion-zsh-stylee/ 
+zstyle -e ':completion::*:*:*:hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 autoload -U compinit
 compinit
 
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,21 +1,11 @@
-# File:     ~/.emacs.d/zsh/path.zsh
-# Author:   Ryan Neufeld <neufelry@gmail.com>
-# Modified: <2009-02-22 18:41:59 CST>
-
 # This file is loaded by zshrc.zsh
 
 setopt ALL_EXPORT
 
-MANPATH=/opt/local/share/man:$MANPATH
-
-# DB bins
-PATH=/usr/local/mysql/bin:$PATH 
+MANPATH=/usr/local/share/man:$MANPATH
 
 # User Dir bins
-PATH=$HOME/bin:$HOME/bin/checker:$PATH
-
-# Port, then homebrew
-PATH="/usr/local/bin:$PATH"
+PATH=$HOME/bin:/usr/local/bin:$PATH
 
 CLASSPATH=.:$CLASSPATH
 unsetopt ALL_EXPORT
