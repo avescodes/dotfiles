7e7a65ba96c0a34498d94494a4d828c451ebd15b
Ryan Neufeld <ryan@thinkrelevance.com>
2012-03-17 23:24:36 -0500

    Removed unnecesary sources

--- zsh/relevance.zsh
+++ zsh/relevance.zsh
@@ -10,15 +10,9 @@ alias tc="decrypt"
 export PATH=~/Development/work/relevance/etc/scripts:$PATH
 
 emulate sh
+
 source ~/Development/work/relevance/etc/bash/aliases.sh
 source ~/Development/work/relevance/etc/bash/git.sh
 source ~/Development/work/relevance/etc/bash/git_prompt.sh
-source ~/Development/work/relevance/etc/bash/rails.sh
-source ~/Development/work/relevance/etc/bash/ruby.sh
-# Do not want.
-# source ~/Development/work/relevance/etc/bash/ssh_autocompletion.sh
-# Don't need yet?
-# source ~/Development/work/Relevance/etc/bash/project_aliases.sh
-# Have via zsh source
-# /Users/jared/Dev/etc/git-completion.bash
+
 emulate zsh
