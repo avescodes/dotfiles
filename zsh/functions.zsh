f0d6f4c7fefe0eff1e420ae620d4fd83997e285b
Ryan Neufeld <ryan@ryans-mbp.local>
2012-03-17 22:55:12 -0500

    Big cleanup of config files

--- zsh/functions.zsh
+++
@@ -1,8 +0,0 @@
-# File:     ~/.emacs.d/zsh/functions.zsh
-# Author:   Ryan Neufeld <neufelry@gmail.com>
-# Modified: <2008-12-13 00:09:41 CST>
-
-mdc()    { mkdir -p "$1" && cd "$1" }
-setenv() { export $1=$2 }
-sdate()  { date +%Y.%m.%d }
-pc()     { awk "{print \$$1}" }
