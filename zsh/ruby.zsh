65a16ae91e4e9343fbb83614094f5590f6328ff5
Ryan Neufeld <ryan@thinkrelevance.com>
2012-03-21 12:03:18 -0500

    Use rvm instead of rbenv

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -6,6 +6,4 @@ export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-
-eval "$(rbenv init -)"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
