3115b0fe2603199279b8e098e0b252641621be9b
Ryan Neufeld <ryan@thinkrelevance.com>
2012-04-10 09:36:50 -0500

    Number of small changes to zsh/misc configs

---
+++ zsh/compile.zsh
@@ -0,0 +1,17 @@
+# https://github.com/spicycode/ze-best-zsh-config/blob/master/.zsh/functions.zsh
+function zsh_recompile {
+  autoload -U zrecompile
+
+  [[ -f ~/.zshrc ]] && zrecompile -p ~/.zshrc
+  [[ -f ~/.zshrc.zwc.old ]] && rm -f ~/.zshrc.zwc.old
+
+  for f in ~/.zsh/**/*.zsh; do
+    [[ -f $f ]] && zrecompile -p $f
+    [[ -f $f.zwc.old ]] && rm -f $f.zwc.old
+  done
+
+  [[ -f ~/.zcompdump ]] && zrecompile -p ~/.zcompdump
+  [[ -f ~/.zcompdump.zwc.old ]] && rm -f ~/.zcompdump.zwc.old
+
+  source ~/.zshrc
+}
--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -6,4 +6,4 @@ export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
+alias rake="noglob rake"
