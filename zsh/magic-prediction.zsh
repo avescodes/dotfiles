9d67a2b5d8d691d2927f9955a4307440737eb166
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-04-17 21:47:28 -0400

    New hotness

---
+++ zsh/magic-prediction.zsh
@@ -0,0 +1,9 @@
+# This just makes things way too real...
+# http://peadrop.com/blog/2007/04/07/welcome-to-mr-crystal-ball/
+
+# autoload predict-on
+# zle -N predict-on
+# zle -N predict-off
+# bindkey '^Z'   predict-on
+# bindkey '^X^Z' predict-off
+# zstyle ':predict' verbose true
