f619338f399d5ff278533477959c09aa589a3c26
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-06-25 21:47:43 -0400

    Welcome back emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,21 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-ruby
+                      starter-kit-js
+                      starter-kit-bindings
+                      )
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
---
+++ emacs.d/ryan.el
@@ -0,0 +1,1 @@
+(global-hl-line-mode -1)
