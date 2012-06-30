e4426c4657547535754a8a4058ef0526b69447f2
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-06-29 22:05:54 -0400

    Solarized color theme for emacs and clojure mode hook

--- emacs.d/ryan.el
+++ emacs.d/ryan.el
@@ -1,1 +1,10 @@
 (global-hl-line-mode -1)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+
+(setq initial-scratch-message nil)
+
+(add-hook 'clojure-mode-hook
+  (lambda ()
+    (setq inferior-lisp-program "lein repl")))
