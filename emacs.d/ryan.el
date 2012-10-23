895459d7a73de7cb18c242f77bc98e13b41a16bd
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-10-23 11:35:13 -0400

    Emacs tweaks

--- emacs.d/ryan.el
+++ emacs.d/ryan.el
@@ -1,21 +1,29 @@
 (global-hl-line-mode -1)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(load-theme 'zenburn t)
+(load-theme 'solarized-dark t)
 
 (setq initial-scratch-message nil)
 
-;; nREPL customizations
-(setq nrepl-popup-stacktraces nil)
-(add-hook 'nrepl-interaction-mode-hook
-          'nrepl-turn-on-eldoc-mode)
+(setq-default tab-width 2)
+(fset 'yes-or-no-p 'y-or-n-p)
 
+;; Make the scratch clojure
 (when (locate-library "clojure-mode")
   (setq initial-major-mode 'clojure-mode))
 
+;; Linum Mode
 (line-number-mode 1) ; have line numbers and
 (column-number-mode 1) ; column numbers in the mode line
-(global-linum-mode 1) ; add line numbers on the left
+(add-hook 'clojure-mode-hook 'linum-mode)
+
+;; RAINBOWS!
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; nREPL customizations
+(setq nrepl-popup-stacktraces nil)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook 'paredit-mode)
 
 (setq x-select-enable-clipboard t
       make-backup-files nil
@@ -23,10 +31,3 @@
       diff-switches "-u -w"
       whitespace-style '(trailing lines space-before-tab
                                   face indentation space-after-tab))
-(setq-default tab-width 2)
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(defcustom clj-dir "/Users/ryan/code/github/clojure"
-  "Path to Clojure source directory."
-  :type 'string
-  :group 'cljrepl)
