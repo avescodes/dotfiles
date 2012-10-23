92d6fd45be2156760ea5422b92c1b0e5d80451e1
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-10-23 11:54:34 -0400

    Hide right prompt in tmux

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -17,9 +17,17 @@ function build-prompt() {
 
     echo "%{$fg[cyan]%}%2c $git $pair$statcolor▸%{$reset_color%} "
 }
+
+function build-right-prompt() {
+  if [ -n $TMUX ]; then
+    echo ""
+  else
+    echo "%{$fg[cyan]%}%n %{$reset_color%}@ %{$fg_bold[green]%}%U%m%u %{$reset_color%}(%T)"
+  fi
+}
 PS1='`build-prompt`'
 
-RPS1="%{$fg[cyan]%}%n %{$reset_color%}@ %{$fg_bold[green]%}%U%m%u %{$reset_color%}(%T)"
+RPS1='`build-right-prompt`'
 
 [ $TERM = "eterm-color" ] && setopt singlelinezle
 [ $TERM = "dumb" ] && unsetopt zle && PS1='$ ' && RPS1=''
