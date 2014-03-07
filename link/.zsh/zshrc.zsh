17f3e12f27cbb13fc2f792579d94b75607e8d68f
Ryan Neufeld <ryan@rkn.io>
2014-03-07 09:16:52 -0500

    Formatting,  misc.

--- link/.zsh/zshrc.zsh
+++ link/.zsh/zshrc.zsh
@@ -30,3 +30,4 @@ command -v gdircolors >/dev/null 2>&1 && eval $( gdircolors -b $HOME/.zsh/LS_COL
 command -v  dircolors >/dev/null 2>&1 && eval $( dircolors -b $HOME/.zsh/LS_COLORS)
 
 [ -f "/opt/boxen/env.sh" ] && source /opt/boxen/env.sh
+
