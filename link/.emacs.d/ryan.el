b46a0898e84b0d6bffa78d2c6974207d6479d479
Ryan Neufeld <ryan@rkn.io>
2014-04-14 11:16:58 -0400

    Define my own hippie-expand-try-functions-list

--- link/.emacs.d/ryan.el
+++ link/.emacs.d/ryan.el
@@ -195,3 +195,16 @@
 (global-set-key (kbd "M-x") 'smex)
 
 (global-set-key (kbd "M-p") 'projectile-find-file)
+
+;; Hippie-expand
+;; ------------
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         ;; try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
